82f25da739cf39f8b6a3057883a0ba97c324eb17
goteam2 <info@goteam2.io>
2025-05-28 16:12:31 -0600

    12

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": require("path").resolve(__dirname, "app"),
+      "@/components": require("path").resolve(__dirname, "app/components"),
+      "@/lib": require("path").resolve(__dirname, "app/lib"),
+      "@/types": require("path").resolve(__dirname, "app/types"),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
